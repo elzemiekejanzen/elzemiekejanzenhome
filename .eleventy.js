7028249e254c70b2f8e0070c2b8066ca64113ead
elzemiekejanzen <elzemiekejanzen@gmail.com>
2026-08-09 01:55:07 +0200

    Mobiel menu en header gefixt
    Mobiel menu en header gefixt

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/CNAME");
+  eleventyConfig.addPassthroughCopy("src/robots.txt");
   eleventyConfig.addPassthroughCopy("src/**/*.pdf");
 
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
