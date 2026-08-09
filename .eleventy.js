2bcb31566a6bbb4d74fbca0b2778abe3f60fa4ab
elzemiekejanzen <elzemiekejanzen@gmail.com>
2026-08-09 17:04:55 +0200

    update bedankt paginas

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/CNAME");
   eleventyConfig.addPassthroughCopy("src/robots.txt");
   eleventyConfig.addPassthroughCopy("src/**/*.pdf");
+  eleventyConfig.addPassthroughCopy({ "src/audio": "audio" });
 
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
   eleventyConfig.addShortcode("buildTime", () => `${Date.now()}`);
